ac63a590dc637bb6106fc71e83722f033686ab51
Vladimir <33979689+AurelVU@users.noreply.github.com>
2021-01-11 23:10:18 +0300

    Как же я заколебался уже с этими вашими ТОИ x2

---
+++ gsv_1.py
@@ -0,0 +1,42 @@
+from statistics import NormalDist
+import numpy as np
+from numpy import linalg as LA
+
+pw1 = 0.1
+pw2 = 0.9
+
+m1 = np.array([2.0, 1.0, 2.0, 2.0, 1.0]).transpose()
+m2 = np.array([1.0, -1.0, 1.0, -1.0, 1.0]).transpose()
+C = np.array([
+    [1.0, 0.1,   0,   0,   0],
+    [0.1, 2.0,   0,   0,   0],
+    [  0,   0, 2.0,   0,   0],
+    [  0,   0,   0, 1.0,   0],
+    [  0,   0,   0,   0, 1.0]
+])
+
+# pw1 = 0.7
+# pw2 = 0.3
+# m1 = np.array([2, 1, -1]).transpose()
+# m2 = np.array([2, -1, 2]).transpose()
+#
+# C = np.array(
+#     [[2, -0.5, -0.3],
+#      [-0.5, 1, -0.5],
+#      [-0.3, -0.5, 1]]
+# )
+
+C_1 = LA.inv(C)
+
+mg1 = (m1 - m2).transpose().dot(C_1).dot(m1 - m2) * 0.5
+h = mg1
+Dg1 = 2 * h
+mg2 = -h
+Dg2 = 2 * h
+
+l0 = pw1 / pw2
+l0_ = np.log(l0)
+print('Ошибка 2 рода:')
+print(NormalDist(mu=mg1, sigma=np.sqrt(Dg1)).cdf(l0_))
+print('Ошибка 1 рода:')
+print(1 - NormalDist(mu=mg2, sigma=np.sqrt(Dg2)).cdf(l0_))
---
+++ naiv_baies.py
@@ -0,0 +1,26 @@
+from statistics import NormalDist
+import numpy as np
+
+# Рассчитать вероятность ошибки распознавания второго рода с
+# использованием наивного байесовского классификатора для n признаков,
+# распределенных по показательному закону (используйте стандартную функцию normcdf)
+# pw1=0.5; pw2=0.5; n=100;
+# L1=1; L2=1.5;
+
+
+pw1 = 0.5
+pw2 = 0.5
+n = 100
+l1 = 1
+l2 = 1.5
+mg1 = n * (np.log(l2 / l1) + l1 * (1 / l2 - 1 / l1))
+Dg1 = n * (l1 ** 2 * (1 / l2 - 1 / l1) ** 2)
+mg2 = n * (np.log(l2 / l1) + l2 * (1 / l2 - 1 / l1))
+Dg2 = n * (l2 ** 2 * (1 / l2 - 1 / l1) ** 2)
+l0 = pw1 / pw2
+l0_ = np.log(l0)
+
+print('Ошибка 1 рода:')
+print(NormalDist(mu=mg1, sigma=np.sqrt(Dg1)).cdf(l0_))
+print('Ошибка 2 рода:')
+print(1 - NormalDist(mu=mg2, sigma=np.sqrt(Dg2)).cdf(l0_))
