c0f8f725ec8a8ce9f761e1c0078480eda1be1024
Vladimir <33979689+AurelVU@users.noreply.github.com>
2021-01-12 11:48:33 +0300

    Исправлена ошибка при рассчете l0

--- gsv_1.py
+++ gsv_1.py
@@ -34,9 +34,9 @@ Dg1 = 2 * h
 mg2 = -h
 Dg2 = 2 * h
 
-l0 = pw1 / pw2
+l0 = pw2 / pw1
 l0_ = np.log(l0)
-print('Ошибка 2 рода:')
-print(NormalDist(mu=mg1, sigma=np.sqrt(Dg1)).cdf(l0_))
 print('Ошибка 1 рода:')
+print(NormalDist(mu=mg1, sigma=np.sqrt(Dg1)).cdf(l0_))
+print('Ошибка 2 рода:')
 print(1 - NormalDist(mu=mg2, sigma=np.sqrt(Dg2)).cdf(l0_))
