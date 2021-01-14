ed03dccc495c7b7f28d69ed6ab345e330a5ba61f
Vladimir <33979689+AurelVU@users.noreply.github.com>
2021-01-14 13:23:52 +0300

    Как же я заколебался уже с этими вашими ТОИ x7

---
+++ isp3.py
@@ -0,0 +1,13 @@
+from math import ceil
+from scipy.stats import norm
+
+
+P_err = 0.1
+P_c = 1 - P_err
+dg = 0.1 * P_err
+gamma = 0.07
+
+t = norm.ppf((2 - gamma) / 2)
+print(t)
+K = ceil(t ** 2 * P_c * (1 - P_c) / dg ** 2)
+print(K)
