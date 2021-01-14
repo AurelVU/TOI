231cec22ae03da208202d1a6e5d2918714b447ff
Vladimir <33979689+AurelVU@users.noreply.github.com>
2021-01-14 10:15:03 +0300

    Как же я заколебался уже с этими вашими ТОИ x5

--- isp.py
+++ isp.py
@@ -1,15 +1,16 @@
 from math import ceil
-from statistics import NormalDist
+from scipy.stats import norm
 
 # Рассчитать гарантированное число испытаний (с округлением до ближайшего целого)
 # для оценки вероятности ошибки при следующих исходных данных: d=0.01, gamma=0.07
 # ВНИМАНИЕ!!! есть задача, где d дается в зависимости от P. Там формула элементарная,
 # перемножить 2 числа. Думаю справитесь
 
+
 dg = 0.01
 gamma = 0.07
 
-t = NormalDist().cdf((2 - gamma) / 2) ** -1
+t = norm.ppf((2 - gamma) / 2)
 print(t)
 K = ceil(t ** 2 / (4 * dg ** 2))
 print(K)
